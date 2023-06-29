eaaa996515c9d7a300067a20784c584d24ea4c13
Thanarat420 <taexxx4@gmal.com>
2023-06-29 22:21:02 +0700

    ้hw2

--- Homework/HW01/hw01-1-helloworld.cpp
+++ Homework/HW01/hw01-1-helloworld.cpp
@@ -1,5 +1,8 @@
-//TODO: #3 provide me full syntax of C-Language contain include, main function(int).
 
-//TODO: #4 display me "Hello World." to me.
+#include <stdio.h>
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
 
-//TODO: #5 Write your private info into Student-Info.txt
